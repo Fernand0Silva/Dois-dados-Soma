011f6a6bd2a1b82adc1b9e39f9cb149c858c2bb6
luiz <luizfernando3995@gmail.com>
2024-04-29 22:07:38 -0300

    projeto dois dados soma

---
+++ View/DiceView.xaml.cs
@@ -0,0 +1,48 @@
+using MetalPerformanceShaders;
+using Microsoft.Maui.Storage;
+using MyticPartyTracker.Models;
+
+namespace MyticPartyTracker.View;
+
+public partial class DiceView : ContentPage
+{
+    public DiceView()
+	{
+		InitializeComponent();
+        DicePicker.SelectedIndex = 0;
+       // QuantityPicker.SelectedIndex = 0;
+
+    }
+    private void DiceBtn_Clicked(object sender, EventArgs e)
+    {
+        AllDicesResultString.Text = " ";
+        int quantidade = Convert.ToInt32(DicePicker.SelectedItem);
+
+        int lados = Convert.ToInt32(SidesPicker.SelectedItem);
+        int total = 0;
+        for(int i =0; i<quantidade;i++)
+        {
+            int resultado = new Dice(lados).Roll();
+            total += resultado; // ou total = total + resultado;
+
+            AllDicesResultString.Text = resultado.ToString() + "\n";//label
+            ResultString.Text = resultado.ToString(); //Label
+
+        }
+      
+    }
+    private void DicePicker_SelectedIndexChanged_1(object sender, EventArgs e)
+    {
+
+    }
+    
+    private void DiceDoisBtn_Clicked(object sender, EventArgs e)
+    {
+       
+    }
+
+    /* private void PicName_SelectedIndexChanged_2(object sender, EventArgs e)
+     {
+
+     }*/
+}
---
+++ ViewModels/DiceViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyticPartyTracker.Models;
+using MyticPartyTracker.ViewModels;
+
+namespace MyticPartyTracker.ViewModels
+{
+   public class DiceViewModel
+    {
+        public DiceViewModel() { 
+        
+        }
+
+        public int numberSides; // numero de lados
+        public int quantity;//contidade
+        public int total;//soma
+
+        public void Roll()
+        {
+            //intancia um novo dado
+            total = 0;
+            Dice dice = new Dice(numberSides);
+
+            for (int i = 0; i < quantity; i++)
+            {
+                int roll = dice.Roll();
+                total += roll;
+            }
+        }
+    }
+}
